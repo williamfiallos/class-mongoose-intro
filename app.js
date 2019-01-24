81a730ed57291a975bac2d91a7153c8eeadd95d7
williamfiallos <wfiallos10@gmail.com>
2019-01-23 21:28:26 -0500

    update CRUD examples

--- app.js
+++ app.js
@@ -18,23 +18,102 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+// CREATE:
 
-Student.create({
-  name: "Ana",
-  course: "UX",
-  startedMonth: "January",
-  startedYear: 2018,
-  projects: [ 'X', 'Y', 'Z' ],
-  previousExperience: false
-})
-.then( newStudent => {
-  console.log("New student successfully created in DB: ", newStudent);
-})
-.catch(err => {
-  console.log("Error while creating new instance: ", err);
-})
+// Student.create({
+//   name: "Paula",
+//   course: "Web Dev",
+//   startedMonth: "October",
+//   startedYear: 2018,
+//   projects: [ 'X', 'Y', 'Z' ],
+//   previousExperience: true
+// })
+// .then( newStudent => {
+//   console.log("New student successfully created in DB: ", newStudent);
+// })
+// .catch(err => {
+//   console.log("Error while creating new instance: ", err);
+// })
 
 
+// ALTERNATIVE WAY TO CREATE INSTANCE IN THE DATABASE
+
+// const camiloInfo = new Student ({
+//   name: "Camilo",
+//   course: "Web Dev",
+//   startedMonth: "December",
+//   startedYear: 2018,
+//   projects: [ 'game' ],
+//   previousExperience: true
+// })
+
+// camiloInfo.save()
+//   .then( newStudentInfo => {
+//     console.log("New student created: ", newStudentInfo)
+//   })
+//   .catch( err => {
+//     console.log("Error while creating new student: ", err)
+//   })
+
+
+// RETRIEVE / READ
+
+// Student.find() // .find() ALWAYS gives back an ARRAY
+//   .then( allStudentFromDB => {
+//     allStudentFromDB.forEach(student => {
+//       console.log(student.name);
+//     })
+//   })
+//   .catch( err => console.log("Error while getting the data from the DB: ", err))
+
+// // .findById() will always give you n OBJECT back
+// Student.findById("5c491ad937c7f812a9ad4ff9")
+//   .then( theStudent => {
+//     console.log("Student is: ", theStudent.name)
+//   })
+//   .catch( err => {
+//     console.log("Err while getting a single student from DB: ", err)
+//   })
+
+
+Student.findOne({ course: "UX" })
+  .then( theStudent => {
+    console.log("Student is: ", theStudent.name)
+  })
+  .catch( err => {
+    console.log("Error while getting a signle student from DB: ", err)
+  })
+
+
+// UPDATE 
+
+// - 1. ID & 2. Object, Field to be updated
+// Student.findByIdAndUpdate("5c491ad937c7f812a9ad4ff9", { name: "Paula S."})
+//   .then( updatedStudent => {
+//     console.log("Updated Student is: ", updatedStudent)
+//   })
+//   .catch( err => {
+//     console.log("Error while updating: ", err)
+//   })
+
+// DELETE
+
+// ------- .findByIdAndDelete or .findByIdAndRemove
+// Student.findByIdAndRemove("5c491ad937c7f812a9ad4ff9")
+//   .then( student => {
+//     console.log(`Student with id: ${student._id} is removed from the DB.`)
+//   })
+//   .catch( err => {
+//     console.log("Error while removing student: ", err)
+//   })
+// --- OR ---
+// Student.findByIdAndDelete("5c4918e5660b09121552ccaa")
+// .then( student => {
+//   console.log(`Student with id: ${student._id} is removed from the DB.`)
+// })
+// .catch( err => {
+//   console.log("Error while removing student: ", err)
+// })
 
 
 app.listen(3000, () => {
